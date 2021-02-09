faafb372e2f3d09b1df42b33aaa4178fb9efcf47
Cguilliman <cguilliman@gmail.com>
2021-02-09 14:19:17 +0200

    Add uploads saving process

--- server/rest/catalog.py
+++ server/rest/catalog.py
@@ -1,6 +1,8 @@
 from fastapi.routing import APIRouter
 from pydantic import BaseModel
-import base64
+
+from base.uploads.models import Image
+from base.uploads.usecases import save_base64_image
 
 
 router = APIRouter(prefix="/api/v1")
@@ -9,11 +11,10 @@ router = APIRouter(prefix="/api/v1")
 class PostItem(BaseModel):
     title: str
     description: str
-    preview: str
+    preview: Image
 
 
 @router.post("/post/create/")
 async def create_post(item: PostItem):
-    print(item)
-    # TODO: validate preview
+    print(await save_base64_image(item.preview))
     return {"status": "OK"}
